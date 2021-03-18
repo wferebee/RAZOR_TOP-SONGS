7b51c010972bb3de865b5dd7947832ad558cf268
DP3PO <williampferebee@gmail.com>
2021-03-18 16:43:06 -0400

    IC
    ENTITY and Visual studio versionsare causing a problem currently and i dont have the heart to try and fix it right now

---
+++ SongsRazor/Controllers/TopSongsController.cs
@@ -0,0 +1,104 @@
+﻿using SongsRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SongsRazor.Controllers
+{
+    public class TopSongsController : Controller
+    {
+        // GET: TopSongs
+        public ActionResult Index()
+        {
+            List<Object> cool  = new List<Object>();
+            using (top_songsdbEntities cty = new top_songsdbEntities())
+            {
+
+                var query = cty.top5000.Where(x => x.position == 16);
+              
+
+                foreach(var z in query)
+                {
+                    cool.Add(z);
+                }
+                ViewBag.Songs = cool;
+            }
+            return View(cool);
+                
+        }
+
+        // GET: TopSongs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TopSongs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TopSongs/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TopSongs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TopSongs/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TopSongs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TopSongs/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SongsRazor/Controllers/top5000Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SongsRazor.Models;
+
+namespace SongsRazor.Controllers
+{
+    public class top5000Controller : Controller
+    {
+        private top_songsdbEntities db = new top_songsdbEntities();
+
+        // GET: top5000
+        public ActionResult Index()
+        {
+            return View(db.top5000.Where(x => x.position == 16).ToList());
+        }
+
+        // GET: top5000/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            top5000 top5000 = db.top5000.Find(id);
+            if (top5000 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(top5000);
+        }
+
+        // GET: top5000/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: top5000/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "position,artist,song,year,raw_total,raw_usa,raw_uk,raw_eur,raw_row")] top5000 top5000)
+        {
+            if (ModelState.IsValid)
+            {
+                db.top5000.Add(top5000);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(top5000);
+        }
+
+        // GET: top5000/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            top5000 top5000 = db.top5000.Find(id);
+            if (top5000 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(top5000);
+        }
+
+        // POST: top5000/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "position,artist,song,year,raw_total,raw_usa,raw_uk,raw_eur,raw_row")] top5000 top5000)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(top5000).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(top5000);
+        }
+
+        // GET: top5000/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            top5000 top5000 = db.top5000.Find(id);
+            if (top5000 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(top5000);
+        }
+
+        // POST: top5000/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            top5000 top5000 = db.top5000.Find(id);
+            db.top5000.Remove(top5000);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
